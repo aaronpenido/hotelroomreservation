0851bfb32894e1387cfd3621a7662559bcae60b8
Aaron Penido <aaron.penido@sonicdrivein.com>
2018-07-11 18:23:36 -0300

    Remove unused import

--- src/test/java/models/PriceCalculatorTest.java
+++ src/test/java/models/PriceCalculatorTest.java
@@ -1,13 +1,13 @@
 package models;
 
 import enums.CustomerType;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import readers.CustomerTypeReader;
 import readers.DateReader;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Arrays;
